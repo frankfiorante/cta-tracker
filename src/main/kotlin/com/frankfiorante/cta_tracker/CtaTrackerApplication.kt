6340f501a0099ffe8fd746b9373db75a6e6da17a
Frank Fiorante <develop@frankfiorante.com>
2026-02-21 22:20:08 -0600

    initial commit

---
+++ src/main/kotlin/com/frankfiorante/cta_tracker/CtaTrackerApplication.kt
@@ -0,0 +1,11 @@
+package com.frankfiorante.cta_tracker
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CtaTrackerApplication
+
+fun main(args: Array<String>) {
+	runApplication<CtaTrackerApplication>(*args)
+}
